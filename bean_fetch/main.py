bd01049bb29f454e80235b5735b85f0cb452306a
David Terry <me@xwvvvvwx.com>
2019-05-26 16:23:56 +0200

    set mypy to strict, configure pytest

--- bean_fetch/main.py
+++ bean_fetch/main.py
@@ -2,12 +2,10 @@ import yaml
 import argparse
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description="Full automated command line bookkeeping")
-parser.add_argument(
-    "-c", "--config", default=Path.cwd(), help="configuration file path"
-)
+parser = argparse.ArgumentParser(description="Fully automated command line bookkeeping")
+parser.add_argument("-c", "--config", help="configuration file path")
 args = parser.parse_args()
 
 
-def main():
-    print("compile!")
+def main() -> None:
+    print(args.config)
