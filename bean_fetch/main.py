448ccaa294483335cfece1f0898405b9ad74e43c
David Terry <me@xwvvvvwx.com>
2019-05-26 16:20:24 +0200

    add config file cli flag

--- bean_fetch/main.py
+++ bean_fetch/main.py
@@ -1,6 +1,13 @@
-import click
+import yaml
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Full automated command line bookkeeping")
+parser.add_argument(
+    "-c", "--config", default=Path.cwd(), help="configuration file path"
+)
+args = parser.parse_args()
 
 
-@click.command()
 def main():
-    click.echo("compile!")
+    print("compile!")
---
+++ readme.md
@@ -0,0 +1,10 @@
+# bean-fetch
+
+`bean-fetch` is a tool that fetches raw transaction data from various financial institutions and transforms it into a single consistent beancount ledger.
+
+## Usage
+
+1. Create a `config.yaml` file
+1. Run `bean-fetch -c <path_to_config>`
+
+## Configuration
