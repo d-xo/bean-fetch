4dd53657c9451ffa31c76462b1afa1e05e2db1ff
David Terry <me@xwvvvvwx.com>
2020-09-15 10:01:58 +0200

    scripts: mk_eth_data: send eth, add uniswap weth pair

--- scripts/mk_eth_data.sh
+++ scripts/mk_eth_data.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+SEP="---------------------------------------------------------------------"
+MAX_UINT=$(seth --to-int256 -1)
+
 # clean up
 trap 'killall geth && rm -rf "$TMPDIR"' EXIT
 trap "exit 1" SIGINT SIGTERM
@@ -78,13 +81,16 @@ function deployToken() {
 
 function msg() {
     echo
-    echo ---------------------------------------------------------------------
+    echo $SEP
     echo "$@"
-    echo ---------------------------------------------------------------------
+    echo $SEP
     echo
 }
 
 msg sending some eth
+sendEth 0xacabacabacabacabacabacabacabacabacabacab "1.5"
+sendEth 0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef "420"
+sendEth 0x4206942069420694206942069420694206942069 "69"
 
 msg deploying uniswap factory
 factory=$(deployUniswapFactory)
@@ -96,39 +102,63 @@ weth=$(deployWeth)
 msg deploying router
 router=$(deployRouter "$factory" "$weth")
 
-msg "creating uniswap pair for $token0 and $token1"
+msg "creating uniswap pair for token0 and token1"
 seth send "$factory" "createPair(address,address)" "$token0" "$token1"
-pair=$(seth call "$factory" "getPair(address,address)(address)" "$token0" "$token1")
+tokenPair=$(seth call "$factory" "getPair(address,address)(address)" "$token0" "$token1")
 
-msg approving the router against both tokens
-seth send "$token0" "approve(address,uint256)" "$router" "$(seth --to-int256 -1)"
-seth send "$token1" "approve(address,uint256)" "$router" "$(seth --to-int256 -1)"
+msg "creating uniswap pair for weth and token1"
+seth send "$factory" "createPair(address,address)" "$weth" "$token1"
+wethPair=$(seth call "$factory" "getPair(address,address)(address)" "$weth" "$token1")
 
-msg approving the router against pair
-seth send "$pair" "approve(address,uint256)" "$router" "$(seth --to-int256 -1)"
+msg approving the router against all tokens
+seth send "$token0" "approve(address,uint256)" "$router" "$MAX_UINT"
+seth send "$token1" "approve(address,uint256)" "$router" "$MAX_UINT"
+seth send "$weth" "approve(address,uint256)" "$router" "$MAX_UINT"
 
-msg adding some liquidity
-seth send "$router" "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" "$token0" "$token1" "$(seth --to-wei 1000 eth)" "$(seth --to-wei 100 eth)" 0 0 "$ETH_FROM" "$(seth --to-int256 -1)"
+msg approving the router against both pairs
+seth send "$tokenPair" "approve(address,uint256)" "$router" "$MAX_UINT"
+seth send "$wethPair" "approve(address,uint256)" "$router" "$MAX_UINT"
 
-msg swapping some tokens
-seth send "$router" "swapExactTokensForTokens(uint,uint,address[],address,uint)" "$(seth --to-wei 10 eth)" 0 "[$token0, $token1]" "$ETH_FROM" "$(seth --to-int256 -1)"
-seth send "$router" "swapExactTokensForTokens(uint,uint,address[],address,uint)" "$(seth --to-wei 60 eth)" 0 "[$token1, $token0]" "$ETH_FROM" "$(seth --to-int256 -1)"
+msg adding some liquidity to the token pair
+seth send "$router" "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" "$token0" "$token1" "$(seth --to-wei 1000 eth)" "$(seth --to-wei 100 eth)" 0 0 "$ETH_FROM" "$MAX_UINT"
 
-msg removing some liquidity
-shares=$(seth call "$pair" "balanceOf(address)(uint)" "$ETH_FROM")
-seth send "$router" "removeLiquidity(address,address,uint,uint,uint,address,uint)" "$token0" "$token1" "$shares" 0 0 "$ETH_FROM" "$(seth --to-int256 -1)"
+msg swapping some tokens on the token pair
+seth send "$router" "swapExactTokensForTokens(uint,uint,address[],address,uint)" "$(seth --to-wei 10 eth)" 0 "[$token0, $token1]" "$ETH_FROM" "$MAX_UINT"
+seth send "$router" "swapExactTokensForTokens(uint,uint,address[],address,uint)" "$(seth --to-wei 60 eth)" 0 "[$token1, $token0]" "$ETH_FROM" "$MAX_UINT"
+
+msg removing some liquidity from the token pair
+shares=$(seth call "$tokenPair" "balanceOf(address)(uint)" "$ETH_FROM")
+seth send "$router" "removeLiquidity(address,address,uint,uint,uint,address,uint)" "$token0" "$token1" "$shares" 0 0 "$ETH_FROM" "$MAX_UINT"
+
+msg adding some liquidity to the weth pair
+seth send --value "$(seth --to-wei 10000 eth)" "$router" "addLiquidityETH(address,uint,uint,uint,address,uint)" "$token1" "$(seth --to-wei 1000 eth)" 0 0 "$ETH_FROM" "$MAX_UINT"
+
+msg swapping some tokens on the weth pair
+seth send --value "$(seth --to-wei 500 eth)" "$router" "swapExactETHForTokens(uint,address[],address,uint)" 0 "[$weth, $token1]" "$ETH_FROM" "$MAX_UINT"
+seth send "$router" "swapExactTokensForETH(uint,uint,address[],address,uint)" "$(seth --to-wei 60 eth)" 0 "[$token1, $weth]" "$ETH_FROM" "$MAX_UINT"
+
+msg removing some liquidity from the weth pair
+shares=$(seth call "$wethPair" "balanceOf(address)(uint)" "$ETH_FROM")
+seth send "$router" "removeLiquidityETH(address,uint,uint,uint,address,uint)" "$token1" "$shares" 0 0 "$ETH_FROM" "$MAX_UINT"
 
 msg running bean-fetch
 beanDir=$(mktemp -d)
-cat > "$beanDir/config.yml" <<EOL
+config="$beanDir/config.yml"
+cat > "$config" <<EOL
 archive_dir: ./archive
 
 ethereum:
   rpc_url: http://127.0.0.1:8545
   start_block: 0
   addresses:
-    - "$(seth --to-address "$ETH_FROM")" # testnet
+    - "0xACABaCAbacAbacABaCaBAcABacABaCaBAcABacab"
+    - "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF"
+    - "0x4206942069420694206942069420694206942069"
+    - "$(seth --to-address "$ETH_FROM")"
 EOL
-python -m bean_fetch.main -c "$beanDir/config.yml"
+python -m bean_fetch.main fetch -c "$config"
 
-msg raw transactions written to "$beanDir/archive"
+echo
+echo $SEP
+echo raw transactions written to "$beanDir/archive"
+echo parse these transactions with: python -m bean_fetch.main parse -c "$config"
